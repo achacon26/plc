8cea881281f7c36a9aba07d3d7caa17ee826e686
Harold Torrado <HighWolf@users.noreply.github.com>
2024-05-01 13:51:11 +1000

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -18,33 +18,33 @@ echo "Installing NodeJS v$node_version..."
 wget https://nodejs.org/dist/v$node_version/node-v$node_version-linux-armv7l.tar.xz
 echo "Unzipping node-v$node_version-linux-armv7l.tar.xz..."
 tar -xJf node-v$node_version-linux-armv7l.tar.xz
-rm node-v$node_version-linux-armv7l.tar.xz
+rm -f node-v$node_version-linux-armv7l.tar.xz
 
 echo "Deploing NodeJS files..."
 rm -Rf $install_folder/local/node
 mv node-v$node_version-linux-armv7l $install_folder/local/node
 rm -Rf node-v$node_version-linux-armv7l
-rm $install_folder/bin/node
+rm -f $install_folder/bin/node
 ln -s $install_folder/local/node/bin/node /usr/bin/node
-rm $install_folder/bin/npm
+rm -f $install_folder/bin/npm
 ln -s $install_folder/local/node/bin/npm /usr/bin/npm
-rm $install_folder/bin/npx
+rm -f $install_folder/bin/npx
 ln -s $install_folder/local/node/bin/npx /usr/bin/npx
 
 echo "Installing libatomic1..."
 
 wget http://ftp.au.debian.org/debian/pool/main/g/gcc-14/libatomic1_14-20240429-1_armhf.deb
 ar x libatomic1_14-20240429-1_armhf.deb
-rm libatomic1_14-20240429-1_armhf.deb
+rm -f libatomic1_14-20240429-1_armhf.deb
 
 echo "Unzipping libatomic..."
 tar -xJf data.tar.xz
-rm data.tar.xz
-rm control.tar.xz
+rm -f data.tar.xz
+rm -f control.tar.xz
 rm -Rf debian-binary
 
 echo "Deploing libatomic1 files..."
-rm $install_folder/lib/libatomic*
+rm -f $install_folder/lib/libatomic*
 mv ./usr/lib/arm-linux-gnueabihf/* $install_folder/lib/
 rm -Rf ./usr
 
