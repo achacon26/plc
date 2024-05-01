d47445867b8b2784f4041b4b86ba238824f44017
Harold Torrado <HighWolf@users.noreply.github.com>
2024-05-01 14:37:57 +1000

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-cd /tmp
+cd /tmp || exit
+workdir /tmp || exit
 
 node_version=$1
 install_folder=$2
 
 if [ -z "$node_version" ]
 then
-    read -p "Please insert the NodeJS version: " node_version
+    read -rp "Please insert the NodeJS version: " node_version
 fi
 
 if [ -z "$install_folder" ]
@@ -16,21 +17,22 @@ fi
 
 echo "Installing NodeJS v$node_version..."
 
-wget https://nodejs.org/dist/v$node_version/node-v$node_version-linux-armv7l.tar.xz
+wget https://nodejs.org/dist/v"$node_version"/node-v"$node_version"-linux-armv7l.tar.xz || exit
 echo "Unzipping node-v$node_version-linux-armv7l.tar.xz..."
-tar -xJf ./node-v$node_version-linux-armv7l.tar.xz
-rm -f node-v$node_version-linux-armv7l.tar.xz
+tar -xJf ./node-v"$node_version"-linux-armv7l.tar.xz || exit
+rm -f node-v"$node_version"-linux-armv7l.tar.xz
 
 echo "Deploing NodeJS files..."
-rm -Rf $install_folder/local/node
-mv ./node-v$node_version-linux-armv7l $install_folder/local/node
-rm -Rf node-v$node_version-linux-armv7l
+rm -Rf "$install_folder"/local/node
+mkdir -p "$install_folder"/local/node || exit
+mv ./node-v"$node_version"-linux-armv7l "$install_folder"/local/node
+rm -Rf node-v"$node_version"-linux-armv7l
 rm -f /usr/bin/node
-ln -s $install_folder/local/node/bin/node /usr/bin/node
+ln -s "$install_folder"/local/node/bin/node /usr/bin/node
 rm -f /usr/bin/npm
-ln -s $install_folder/local/node/bin/npm /usr/bin/npm
+ln -s "$install_folder"/local/node/bin/npm /usr/bin/npm
 rm -f /usr/bin/npx
-ln -s $install_folder/local/node/bin/npx /usr/bin/npx
+ln -s "$install_folder"/local/node/bin/npx /usr/bin/npx
 
 echo "Installing libatomic1..."
 
@@ -46,8 +48,14 @@ rm -Rf debian-binary
 
 echo "Deploing libatomic1 files..."
 rm -f /usr/lib/libatomic*
-mv ./usr/lib/arm-linux-gnueabihf/* /usr/lib/
+mkdir -p "$install_folder"/lib || exit
+mv ./usr/lib/arm-linux-gnueabihf/* "$install_folder"/lib/ || exit
 rm -Rf ./usr
+
+if [ "$install_folder/lib/" != "/usr/lib" ]
+then
+    ln -s "$install_folder"/lib/libatomic.so.1.2.0 /usr/lib/libatomic.so.1
+fi
 
 echo "Checking NodeJS version:"
 node -v
